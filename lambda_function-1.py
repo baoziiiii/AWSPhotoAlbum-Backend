f276e360fb0f7cffbfab8f17411af9a6901899ed
RyanBao <ryanbao@Ryans-Air.fios-router.home>
2020-05-01 02:13:12 -0400

    a

---
+++ lambda_function-1.py
@@ -0,0 +1,119 @@
+import json
+import boto3
+import os
+import time
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+
+
+def detect_labels(photo, bucket):
+
+    client=boto3.client('rekognition')
+
+    response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':photo}},
+        MaxLabels=10)
+    #print (response)
+    # print('Detected labels for ' + photo) 
+    # print()   
+    # for label in response['Labels']:
+    #     print ("Label: " + label['Name'])
+    #     print ("Confidence: " + str(label['Confidence']))
+    #     print ("Instances:")
+    #     for instance in label['Instances']:
+    #         print ("  Bounding box")
+    #         print ("    Top: " + str(instance['BoundingBox']['Top']))
+    #         print ("    Left: " + str(instance['BoundingBox']['Left']))
+    #         print ("    Width: " +  str(instance['BoundingBox']['Width']))
+    #         print ("    Height: " +  str(instance['BoundingBox']['Height']))
+    #         print ("  Confidence: " + str(instance['Confidence']))
+    #         print()
+
+    #     print ("Parents:")
+    #     for parent in label['Parents']:
+    #         print ("   " + parent['Name'])
+    #     print ("----------")
+    #     print ()
+    #return len(response['Labels'])
+    res = []
+    for item in response['Labels']:
+        res.append(item['Name'])
+    return res
+
+
+
+def lambda_handler(event, context):
+    print(event)
+    # get bucket and photo name
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    photo = event['Records'][0]['s3']['object']['key']
+    # photo='images/orange_cat_1-1588193841880.jpg'
+    # bucket='p3b2'
+    
+    # tag photo, store result as list
+    labels_res = detect_labels(photo,bucket)
+    # try:
+    #     labels_res = detect_labels(photo,bucket)
+    # except Exception as err:
+    #     print("cannot tag!",err)
+    
+    print(labels_res)
+    
+    # print("## variables:")
+    # print(os.environ) 
+    # print('## Event')
+    # print(event)
+    
+    # connect to vpc
+    # host = 'vpc-photos2-wc7ewp7gezc7jkxe5sumk4wxni.us-east-1.es.amazonaws.com' 
+    host = 'vpc-photos4-qircgu67ftakri6xjsbsuvwqca.us-east-1.es.amazonaws.com'
+    region = 'us-east-1' # e.g. us-west-1
+    
+    # connect to es
+    service = 'es'
+    credentials = boto3.Session().get_credentials()
+    awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+    
+    es = Elasticsearch(
+        hosts = [{'host': host, 'port': 443}],
+        http_auth = awsauth,
+        use_ssl = True,
+        verify_certs = True,
+        connection_class = RequestsHttpConnection
+    )
+    
+    # create doc
+    document = {
+        "objectkey": photo[7:],
+        "bucket": bucket,
+        "createdTimestamp": time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime()),
+        "labels": labels_res
+    }
+    
+    print(document)
+    
+    # #clear es
+    # try: 
+    #     es.indices.delete(index='photos')
+    # except:
+    #     pass
+    # # es.indices.delete(index='movies')
+
+    # es.indices.create(index='photos')
+
+    # try: 
+    #     es.indices.refresh(index='photos')
+    # except:
+    #     pass
+    
+    # insert doc to es
+    response = es.index(index="photos", doc_type="_doc", id=photo[7:] , body=document)
+    print (response)
+
+    
+    return {
+        'statusCode': 200,
+        'body': str(response)
+    }
+    
+    
+    
---
+++ lambda_function.py
@@ -0,0 +1,133 @@
+import json
+import boto3
+import base64
+import os
+import time
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+
+
+s3 = boto3.client("s3")
+
+
+def list_picture(key,labels): 
+    r = {}
+    # label = ''.join(key.rsplit(key.split("-")[-1],1))[:-1].replace('images/','',1) # TOCHANGE: label暂时是从文件名提取
+    # r['Label'] = label
+    
+    # concat labels list to string
+    label_string = ','.join(labels)
+    r['Label'] = label_string
+    # replace blank space to +
+    key = key.replace(" ", "+")
+    r['Path'] = 'https://p3b2.s3.amazonaws.com/images/'+key
+    return r
+
+
+
+## IMPORTANT: get picture raw binary data
+def get_raw_picture(key):
+    r =  s3.get_object(
+        Bucket='p3b2',
+        Key = key
+    )
+    if r and r['Body']:
+        return base64.b64decode(str(r['Body'].read()).split(',')[-1])
+    return None
+
+
+def list_all_pictures(prefix = 'images/',maxkeys = 100, reset = True, key_word=''):
+    if reset:
+        del images[:]
+    # response = s3.list_objects(
+    #     Bucket='p3b2',
+    #     MaxKeys=maxkeys,
+    #     Prefix=prefix,
+    # )
+    # for content in response["Contents"]:
+    #     key = content["Key"]
+    #     if key != 'images/':
+    #         images.append(list_picture(key))
+    
+    # connect to vpc
+    host = 'vpc-photos2-wc7ewp7gezc7jkxe5sumk4wxni.us-east-1.es.amazonaws.com' 
+    region = 'us-east-1' # e.g. us-west-1
+    
+    # connect to es
+    service = 'es'
+    credentials = boto3.Session().get_credentials()
+    awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+    
+    es = Elasticsearch(
+        hosts = [{'host': host, 'port': 443}],
+        http_auth = awsauth,
+        use_ssl = True,
+        verify_certs = True,
+        connection_class = RequestsHttpConnection
+    )
+    
+   
+    # change key_word to from list to string
+    #search_word=' '.join(key_word)
+    # search key_word as tag 
+    query_body = {
+      "size": 20,
+      "query": {
+        "query_string": {
+          "query": key_word,
+          "default_field": "labels"
+        }
+      }
+    }
+    
+    # check all records in es
+    #print(es.indices.get_alias("*") )
+    
+    # search tag
+    res = es.search(index="photos", body=query_body)
+    #print (res)
+    
+   # for content in response["Contents"]:
+        # key = content["Key"]
+        # if key != 'images/':
+    for records in res['hits']['hits']:
+        key = records['_source']['objectkey']
+        #print(key)
+        labels = records['_source']['labels']
+        images.append(list_picture(key,labels))
+        
+    # print("images:",images)
+    return images
+
+images = [] 
+
+def lambda_handler(event, context):
+    
+    print(event)
+    
+    # TODO implement
+    # print(event)
+
+
+    query_text = event["queryStringParameters"]["q"]
+    #print(query_text) # it's query string
+    
+    search_res = list_all_pictures(reset=True,key_word=query_text)
+    
+    print("all search photos:",images)
+    
+    returnBody = {
+        "imagePaths":images
+    }
+    
+    return {
+        'statusCode': 200,
+        'headers': {
+            "Content-Type" : "application/json",
+            "Access-Control-Allow-Origin" : "*",
+            "Allow" : "GET, OPTIONS, POST",
+            "Access-Control-Allow-Methods" : "GET, OPTIONS, POST",
+            "Access-Control-Allow-Headers" : "*"
+        },
+        'body': str(returnBody)
+    }
