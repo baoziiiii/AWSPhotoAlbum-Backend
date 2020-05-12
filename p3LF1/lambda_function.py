4de1cd8fcb49bf0fa0951c51280d9a704c570fcb
RyanBao <ryanbao@Ryans-Air.fios-router.home>
2020-05-12 02:07:16 -0400

    a

--- p3LF1/lambda_function.py
+++ p3LF1/lambda_function.py
@@ -12,7 +12,6 @@ def detect_labels(photo, bucket):
     response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':photo}},
         MaxLabels=10)
 
-        
     # print (response)
     # print('Detected labels for ' + photo) 
     # print()   
